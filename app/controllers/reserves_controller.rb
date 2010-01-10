1b1d6bd67361986db3d4f22fb11e12055fcf75a0
Alexandre da Silva <lexrupy@gmail.com>
2010-01-10 15:29:20 -0200

    Corrigir erro de digitacao commit anterior + traducao mensagens de erro

--- app/controllers/reserves_controller.rb
+++ app/controllers/reserves_controller.rb
@@ -9,6 +9,11 @@ class ReservesController < ApplicationController
   end
   
   def create
+    @last_reserve = current_user.reserves.last(:order => 'created_at DESC')
+    if @last_reserve.created_at > 30.seconds.ago
+      flash[:notice] = "Você já fez uma reserva a menos de 30 segundos... por favor, aguarde o processamento da reserva anterior antes de continuar."
+      return redirect_to reserves_path
+    end
     @reserve = Reserve.new(params[:reserve])
     @reserve.user = current_user
     if @reserve.save
