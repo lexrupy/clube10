51e33ffa6f74a1cecfe6cd73140ad2d469f9582c
Alexandre da Silva <lexrupy@gmail.com>
2010-02-28 17:22:28 -0300

    added variables

--- certs/apache_passenger.sh
+++ certs/apache_passenger.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 # Script for configure apache in my virtualbox Ubuntu 8.04 server
-echo "LoadModule passenger_module /usr/lib/ruby/gems/1.8/gems/passenger-2.2.10/ext/apache2/mod_passenger.so" > /etc/apache2/mods-available/passenger.load
-echo "PassengerRoot /usr/lib/ruby/gems/1.8/gems/passenger-2.2.10" > /etc/apache2/mods-available/passenger.conf
-echo "PassengerRuby /usr/bin/ruby1.8" >> /etc/apache2/mods-available/passenger.conf
 
-echo "<VirtualHost *:80>" > /etc/apache2/sites-available/clube10.conf
-echo "  ServerName clube10.local" >> /etc/apache2/sites-available/clube10.conf
-echo "  DocumentRoot /home/administrador/clube10/public" >> /etc/apache2/sites-available/clube10.conf
-echo "  <Directory /home/administrador/clube10/public>" >> /etc/apache2/sites-available/clube10.conf
-echo "    AllowOverride all" >> /etc/apache2/sites-available/clube10.conf
-echo "    Options -MultiViews" >> /etc/apache2/sites-available/clube10.conf
-echo "  </Directory>" >> /etc/apache2/sites-available/clube10.conf
-echo "</VirtualHost>" >> /etc/apache2/sites-available/clube10.conf
+loadfile=/etc/apache2/mods-available/passenger.load
+conffile=/etc/apache2/mods-available/passenger.conf
+sitefile=/etc/apache2/sites-available/clube10
 
-echo "<VirtualHost *:443>" > /etc/apache2/sites-available/clube10.conf
-echo "  ServerName clube10.local" >> /etc/apache2/sites-available/clube10.conf
-echo "  DocumentRoot /home/administrador/clube10/public" >> /etc/apache2/sites-available/clube10.conf
-echo "  <Directory /home/administrador/clube10/public>" >> /etc/apache2/sites-available/clube10.conf
-echo "    AllowOverride all" >> /etc/apache2/sites-available/clube10.conf
-echo "    Options -MultiViews" >> /etc/apache2/sites-available/clube10.conf
-echo "  </Directory>" >> /etc/apache2/sites-available/clube10.conf
-echo "  SSLEngine on" >> /etc/apache2/sites-available/clube10.conf
-echo "  SSLCertificateFile /home/administrador/clube10/certs/clube10.local.crt" >> /etc/apache2/sites-available/clube10.conf
-echo "  SSLCertificateKeyFile /home/administrador/clube10/certs/clube10.local.key" >> /etc/apache2/sites-available/clube10.conf
-echo "</VirtualHost>" >> /etc/apache2/sites-available/clube10.conf
+echo "LoadModule passenger_module /usr/lib/ruby/gems/1.8/gems/passenger-2.2.10/ext/apache2/mod_passenger.so" > $loadfile
+echo "PassengerRoot /usr/lib/ruby/gems/1.8/gems/passenger-2.2.10" > $conffile
+echo "PassengerRuby /usr/bin/ruby1.8" >> $conffile
+
+echo "<VirtualHost *:80>" > $sitefile
+echo "  ServerName clube10.local" >> $sitefile
+echo "  DocumentRoot /home/administrador/clube10/public" >> $sitefile
+echo "  <Directory /home/administrador/clube10/public>" >> $sitefile
+echo "    AllowOverride all" >> $sitefile
+echo "    Options -MultiViews" >> $sitefile
+echo "  </Directory>" >> $sitefile
+echo "</VirtualHost>" >> $sitefile
+
+echo "<VirtualHost *:443>" > $sitefile
+echo "  ServerName clube10.local" >> $sitefile
+echo "  DocumentRoot /home/administrador/clube10/public" >> $sitefile
+echo "  <Directory /home/administrador/clube10/public>" >> $sitefile
+echo "    AllowOverride all" >> $sitefile
+echo "    Options -MultiViews" >> $sitefile
+echo "  </Directory>" >> $sitefile
+echo "  SSLEngine on" >> $sitefile
+echo "  SSLCertificateFile /home/administrador/clube10/certs/clube10.local.crt" >> $sitefile
+echo "  SSLCertificateKeyFile /home/administrador/clube10/certs/clube10.local.key" >> $sitefile
+echo "</VirtualHost>" >> $sitefile
