c37df81c14f548cab0514a86be93c0fd9b0e9710
Alexandre da Silva <lexrupy@gmail.com>
2010-02-28 17:02:21 -0300

    Little update

--- certs/apache_passenger.sh
+++ certs/apache_passenger.sh
@@ -5,7 +5,7 @@ echo "PassengerRoot /usr/lib/ruby/gems/1.8/gems/passenger-2.2.10" > /etc/apache2
 echo "PassengerRuby /usr/bin/ruby1.8" >> /etc/apache2/mods-available/passenger.conf
 
 echo "<VirtualHost *:80>" > /etc/apache2/mods-available/clube10.conf
-echo "  ServerName clube10.siverti.com.br" >> /etc/apache2/mods-available/clube10.conf
+echo "  ServerName clube10.local" >> /etc/apache2/mods-available/clube10.conf
 echo "  DocumentRoot /home/administrador/clube10/public" >> /etc/apache2/mods-available/clube10.conf
 echo "  <Directory /home/administrador/clube10/public>" >> /etc/apache2/mods-available/clube10.conf
 echo "    AllowOverride all" >> /etc/apache2/mods-available/clube10.conf
@@ -13,3 +13,14 @@ echo "    Options -MultiViews" >> /etc/apache2/mods-available/clube10.conf
 echo "  </Directory>" >> /etc/apache2/mods-available/clube10.conf
 echo "</VirtualHost>" >> /etc/apache2/mods-available/clube10.conf
 
+echo "<VirtualHost *:443>" > /etc/apache2/mods-available/clube10.conf
+echo "  ServerName clube10.local" >> /etc/apache2/mods-available/clube10.conf
+echo "  DocumentRoot /home/administrador/clube10/public" >> /etc/apache2/mods-available/clube10.conf
+echo "  <Directory /home/administrador/clube10/public>" >> /etc/apache2/mods-available/clube10.conf
+echo "    AllowOverride all" >> /etc/apache2/mods-available/clube10.conf
+echo "    Options -MultiViews" >> /etc/apache2/mods-available/clube10.conf
+echo "  </Directory>" >> /etc/apache2/mods-available/clube10.conf
+echo "  SSLEngine on" >> /etc/apache2/mods-available/clube10.conf
+echo "  SSLCertificateFile /home/administrador/clube10/certs/clube10.local.crt" >> /etc/apache2/mods-available/clube10.conf
+echo "  SSLCertificateKeyFile /home/administrador/clube10/certs/clube10.local.key" >> /etc/apache2/mods-available/clube10.conf
+echo "</VirtualHost>" >> /etc/apache2/mods-available/clube10.conf
